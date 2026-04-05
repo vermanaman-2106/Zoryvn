c0f9b774c68f4dfcfa73305a33d53c2bc2381fb2
vermanaman-2106 <vermanaman2106@gmail.com>
2026-04-06 01:49:36 +0530

    updated apiurl

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
